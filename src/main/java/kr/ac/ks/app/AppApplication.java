ad7f5625cbdf87b4b4f6fbd9524cbbacbe873912
qwer8714 <37928563+qwer8714@users.noreply.github.com>
2020-06-18 16:08:49 +0900

    Report#3_2 remainingQuota 변수 추가

--- src/main/java/kr/ac/ks/app/AppApplication.java
+++ src/main/java/kr/ac/ks/app/AppApplication.java
@@ -23,8 +23,9 @@ public class AppApplication {
 		return (args) -> {
 			studentRepository.save(Student.builder().name("1").email("1@1.com").build());
 			studentRepository.save(Student.builder().name("2").email("2@2.com").build());
-			lessonRepository.save(Lesson.builder().name("L1").quota(15).build());
-			lessonRepository.save(Lesson.builder().name("L2").quota(15).build());
+			lessonRepository.save(Lesson.builder().name("L1").quota(15).remainingQuota(15).build());
+			lessonRepository.save(Lesson.builder().name("L2").quota(15).remainingQuota(15).build());
+			lessonRepository.save(Lesson.builder().name("L3").quota(1).remainingQuota(1).build());
 		};
 	}
 
